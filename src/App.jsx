1cecd9877d37f42d3af1fe8166b46e48596265d6
Ali <aliazadigithub@gmail.com>
2025-02-08 15:58:23 +0330

    spread in props

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,48 @@ import "./App.css";
 import Prodect from "./Components/Prodect";
 
 function App() {
+  const Allproducts = [
+    {
+      id: 1,
+      image: "../../public/Images/p1.webp",
+      title: "Product 1",
+      price: 23000,
+      details:
+        " Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinction",
+      count: 21,
+    },
+    {
+      id: 2,
+      image: "../../public/Images/p2.webp",
+      title: "Product 2",
+      price: 48000,
+      details:
+        "magni exercitationem quo quidem. Dolore repellat possimus obcaecatin",
+      count: 21,
+    },
+    {
+      id: 3,
+      image: "../../public/Images/p3.webp",
+      title: "Product 3",
+      price: 96000,
+      details: "exercitationem quae pariatur repellendus natus consequuntur.",
+      count: 21,
+    },
+  ];
+
   return (
     <>
       <h1> Main Page</h1>
       <div className="cont">
-        <Prodect
-          image="../../public/Images/p1.webp"
-          title="Product 1"
-          price={23000}
-          details=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinction"
-        />
-        <Prodect
-          image="../../public/Images/p2.webp"
-          title="Product 2"
-          price={48000}
-          details="        magni exercitationem quo quidem. Dolore repellat possimus obcaecati
-        expedita! Reprehenderit eos numquam, praesentium aliquid deserunt"
-        />
-        <Prodect
-          image="../../public/Images/p3.webp"
-          title="Product 3"
-          price={96000}
-          details="exercitationem quae pariatur repellendus natus consequuntur."
-        />
+        <Prodect {...Allproducts[0]} />
+        <Prodect {...Allproducts[1]} />
+        <Prodect {...Allproducts[2]} />
+        <Prodect {...Allproducts[0]} />
+        <Prodect {...Allproducts[1]} />
+        <Prodect {...Allproducts[2]} />
+        <Prodect {...Allproducts[0]} />
+        <Prodect {...Allproducts[1]} />
+        <Prodect {...Allproducts[2]} />
       </div>
     </>
   );
--- src/Components/Prodect.jsx
+++ src/Components/Prodect.jsx
@@ -1,17 +1,36 @@
 import React from "react";
 import "./Product.css";
 const Prodect = (props) => {
+  let { image, title, price, details, count } = props; //object destructuring!
   return (
-    <div className="container">
-      <img src={props.image} alt="" />
-      <div className="title">{props.title}</div>
-      <div className="price">{props.price}</div>
-      <div className="details">{props.details}</div>
-      <div className="buy">
-        <button>buy</button>
+    props.count && (
+      <div className="container">
+        <img src={image} alt="" />
+        <div className="title">{title}</div>
+        <div className="price">{price}</div>
+        <div className="details">{details}</div>
+        <div className="buy">
+          <button>buy</button>
+        </div>
       </div>
-    </div>
+    )
   );
 };
+
+// const Prodect = ({ image, title, price, details, count } ) => {
+//   return (
+//     props.count && (
+//       <div className="container">
+//         <img src={image} alt="" />
+//         <div className="title">{title}</div>
+//         <div className="price">{price}</div>
+//         <div className="details">{details}</div>
+//         <div className="buy">
+//           <button>buy</button>
+//         </div>
+//       </div>
+//     )
+//   );
+// };
 
 export default Prodect;
